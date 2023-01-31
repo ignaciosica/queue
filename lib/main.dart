7b2dd7700c8fe05013cc0ff1f49bd9100581d847
P4ssenger <p4ssenger-developer@users.noreply.github.com>
2023-01-31 17:00:25 -0300

    [#20] background process isolate first test working

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'dart:isolate';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:groupify/app/app.dart';
 import 'package:groupify/auth/auth.dart';
@@ -9,7 +13,9 @@ import 'package:groupify/common/common.dart';
 import 'package:groupify/firebase_options.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:spotify_sdk/spotify_sdk.dart';
 import 'package:workmanager/workmanager.dart';
+
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -26,7 +32,34 @@ void main() {
       Workmanager().initialize(callbackDispatcher, isInDebugMode: true);
       Workmanager().cancelAll();
 
+      RootIsolateToken rootIsolateToken = RootIsolateToken.instance!;
+      Isolate.spawn(_isolateMain, rootIsolateToken);
+
       runApp(App(authenticationRepository: authenticationRepository, firestoreRepository: firestoreRepository));
     },
   );
+}
+
+void _isolateMain(RootIsolateToken rootIsolateToken) async {
+  // Register the background isolate with the root isolate.
+  BackgroundIsolateBinaryMessenger.ensureInitialized(rootIsolateToken);
+  try {
+    await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+    await SpotifySdk.connectToSpotifyRemote(clientId: 'b9a4881e77f4488eb882788cb106a297', redirectUrl: 'https://com.example.groupify/callback/');
+  } catch (e) {
+    print(e);
+  }
+
+  int a = 0;
+
+  while(true){
+    await Future.delayed(const Duration(seconds: 1));
+    print(a++);
+    SpotifySdk.getPlayerState().then((value) => (value?.isPaused ?? false )? SpotifySdk.resume() : SpotifySdk.pause());
+  }
+
+  // You can now use the shared_preferences plugin.
+  // SharedPreferences sharedPreferences =
+  // await SharedPreferences.getInstance();
+  // print(sharedPreferences.getBool(‘isDebug’));
 }
