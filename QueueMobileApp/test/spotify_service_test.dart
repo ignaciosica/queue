2206447a4b5c67b1cdd394fef605beeb095ef79c
p4sscode <p4ssenger.developer@gmail.com>
2023-08-14 11:19:40 -0300

    [dev] add spotify service test

--- QueueMobileApp/test/spotify_service_test.dart
+++ QueueMobileApp/test/spotify_service_test.dart
@@ -1,1 +1,19 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:queue/services/spotify_service.dart';
 
+void main() async {
+  late ISpotifyService spotifyService;
+
+  setUp(() async {
+    await dotenv.load(fileName: '.env');
+    spotifyService = SpotifyService();
+  });
+
+  group('search', () {
+    test('should return a list of tracks', () async {
+      final tracks = await spotifyService.search('test');
+      expect(tracks, isA<List>());
+    });
+  });
+}
