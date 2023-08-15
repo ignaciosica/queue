69310199d1e46f52bc8ec3e766e64cfc7389ee33
p4sscode <p4ssenger.developer@gmail.com>
2023-08-15 12:53:21 -0300

    [dev] polish background task

--- QueueMobileApp/lib/app/background_task.dart
+++ QueueMobileApp/lib/app/background_task.dart
@@ -1,44 +1,97 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:queue/firebase_options.dart';
+import 'package:spotify_sdk/models/player_state.dart';
 import 'package:spotify_sdk/spotify_sdk.dart';
 import 'package:workmanager/workmanager.dart';
 
-//TODO: listen room stream and check for skip and queue
-
 @pragma('vm:entry-point')
 void callbackDispatcher() {
+  late final DocumentReference<dynamic> roomRef;
+  late final Stream<PlayerState> playerStream;
+  late final Stream<DocumentSnapshot<dynamic>> roomStream;
+
   Workmanager().executeTask((taskName, inputData) async {
-    await dotenv.load(fileName: '.env');
-    await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+    final roomId = await initTask(inputData);
 
-    if (kDebugMode) print('roomId:${inputData!['roomId']}');
+    roomRef = FirebaseFirestore.instance.collection('rooms').doc(roomId);
 
-    try {
-      final clientId = dotenv.get('SPOTIFY_CLIENT_ID');
-      if (kDebugMode) print('clientId:$clientId');
-
-      final redirectUrl = dotenv.get('SPOTIFY_REDIRECT_URI');
-      if (kDebugMode) print('redirect:$redirectUrl');
-
-      await SpotifySdk.connectToSpotifyRemote(clientId: clientId, redirectUrl: redirectUrl);
-    } on Exception catch (e) {
-      if (kDebugMode) print('error:$e');
-    }
-
-    final playerStream = SpotifySdk.subscribePlayerState();
-
-    final roomRef = FirebaseFirestore.instance.collection('rooms').doc(inputData!['roomId']);
-    final roomStream = roomRef.snapshots();
+    playerStream = SpotifySdk.subscribePlayerState();
+    roomStream = roomRef.snapshots();
 
     playerStream.listen((playerState) async {
-      if (playerState.track == null) {
-        await roomRef.update({'player_state': null});
-      } else {
-        await roomRef.update({
+      await roomRef.update({'player_state': playerStateToJson(playerState)});
+    });
+
+    roomStream.listen((event) async {
+      final data = event.data()!;
+
+      if (data['player'] != FirebaseAuth.instance.currentUser!.uid) {
+        SpotifySdk.pause();
+        Workmanager().cancelAll();
+      }
+
+      if (data['skip'].length >= (data['participants'].length / 2)) {
+        await queue(roomRef);
+        SpotifySdk.skipNext();
+      }
+
+      switch (data['task']['requested_action']) {
+        case 'pause':
+          SpotifySdk.pause();
+          break;
+        case 'play':
+          SpotifySdk.resume();
+          break;
+        case 'kill':
+          SpotifySdk.pause();
+          Workmanager().cancelAll();
+          break;
+      }
+
+      await roomRef.update({'task.requested_action': null});
+    });
+
+    while (true) {
+      await Future.delayed(const Duration(seconds: 3));
+
+      final ps = await SpotifySdk.getPlayerState();
+
+      if (ps == null || ps.track == null) continue;
+
+      if (ps.track!.duration - ps.playbackPosition < 7 * 1000) {
+        await queue(roomRef);
+        await Future.delayed(const Duration(seconds: 8));
+      }
+    }
+  });
+}
+
+Future<String> initTask(inputData) async {
+  try {
+    await dotenv.load(fileName: '.env');
+    final clientId = dotenv.get('SPOTIFY_CLIENT_ID');
+    final redirectUrl = dotenv.get('SPOTIFY_REDIRECT_URI');
+
+    await SpotifySdk.connectToSpotifyRemote(clientId: clientId, redirectUrl: redirectUrl);
+
+    await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+    final roomId = inputData?['roomId'];
+    if (roomId == null) throw Exception('task_e: roomId is null');
+
+    return roomId;
+  } on Exception catch (e) {
+    throw Exception('task_e: init failed with ($e)');
+  }
+}
+
+Map? playerStateToJson(playerState) {
+  return playerState.track == null
+      ? null
+      : {
           'player_state.duration': playerState.track!.duration,
           'player_state.playback_position': playerState.playbackPosition,
           'player_state.is_paused': playerState.isPaused,
@@ -46,74 +99,29 @@ void callbackDispatcher() {
           'player_state.uri': playerState.track!.uri,
           'player_state.image_uri': playerState.track!.imageUri.raw,
           'player_state.artists': playerState.track!.artists.map((e) => e.name).toList(),
-        });
-      }
-    });
+        };
+}
 
-    queue() async {
-      final query = roomRef
-          .collection('queue')
-          .orderBy('votes', descending: true)
-          .orderBy('created_at', descending: false)
-          .limit(1);
+Future<dynamic> getHead(roomRef) async {
+  final snapshot = await roomRef
+      .collection('queue')
+      .orderBy('votes', descending: true)
+      .orderBy('created_at', descending: false)
+      .limit(1)
+      .get();
 
-      final snapshot = (await query.get());
+  if (snapshot.docs.isEmpty) return null;
 
-      roomRef.update({'skip': []});
+  return snapshot.docs[0];
+}
 
-      if (snapshot.docs.isEmpty) return;
+Future<void> queue(roomRef) async {
+  roomRef.update({'skip': []});
 
-      final track = snapshot.docs[0];
-
-      await roomRef.collection('history').doc(track.id).set(track.data());
-      SpotifySdk.queue(spotifyUri: track.id);
-      await roomRef.collection('queue').doc(track.id).delete();
-    }
-
-    roomStream.listen((event) async {
-      final data = event.data()!;
-      if (data['player'] != FirebaseAuth.instance.currentUser!.uid) {
-        SpotifySdk.pause();
-        Workmanager().cancelAll();
-      }
-
-      if (data['skip'].length >= (data['participants'].length / 2)) {
-        await queue.call();
-        SpotifySdk.skipNext();
-      }
-
-      switch (data['task']['requested_action']) {
-        case 'pause':
-          SpotifySdk.pause();
-          await roomRef.update({'task.requested_action': null});
-          break;
-        case 'play':
-          SpotifySdk.resume();
-          await roomRef.update({'task.requested_action': null});
-          break;
-        case 'kill':
-          SpotifySdk.pause();
-          await roomRef.update({'task.requested_action': null});
-          Workmanager().cancelAll();
-          break;
-        default:
-      }
-    });
-
-    int a = 0;
-
-    while (true) {
-      await Future.delayed(const Duration(seconds: 3));
-      if (kDebugMode) print('a:${a++}');
-
-      final playerState = await SpotifySdk.getPlayerState();
-
-      if (playerState == null || playerState.track == null) continue;
-
-      if (playerState.track!.duration - playerState.playbackPosition < 7 * 1000) {
-        await queue.call();
-        await Future.delayed(const Duration(seconds: 10));
-      }
-    }
-  });
+  final head = await getHead(roomRef);
+  if (head != null) {
+    await roomRef.collection('history').doc(head.id).set(head.data());
+    await roomRef.collection('queue').doc(head.id).delete();
+    await SpotifySdk.queue(spotifyUri: head.id);
+  }
 }
